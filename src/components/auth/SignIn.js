75df0c5ffc17ef47b6d840680fc269c2dd1dfa31
Samitti <samuelteweldebrhan@gmail.com>
2021-02-24 11:14:22 +0300

    add redirect after sign in

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,14 +1,27 @@
 import * as React from 'react';
 import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import UserCreate from '../../redux/actions/signInActions';
 import './form.css';
 
 export default function SignIn() {
   const { register, handleSubmit } = useForm();
   const dispatch = useDispatch();
+  const history = useHistory();
+  useSelector(state => state.User.data.data);
+  let loggedInUser = '';
+  const login = () => {
+    setTimeout(() => {
+      loggedInUser = localStorage.getItem('user');
+      if (loggedInUser) {
+        history.push('/cars');
+      }
+    }, 2000);
+  };
   const onSubmit = data => {
     dispatch(UserCreate(data));
+    login();
   };
 
   return (
@@ -20,6 +33,7 @@ export default function SignIn() {
         <input name="password" type="password" ref={register} placeholder="Password" />
         <input type="submit" />
       </form>
+      {login}
     </div>
   );
 }
